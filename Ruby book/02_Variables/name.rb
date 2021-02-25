4c7b01335e07289e842c748d7446b4a2a2b5af8e
PedroLages <pedrocamposlages@gmail.com>
2021-02-25 18:35:46 +0100

    Complete the name.rb exercise

--- Ruby book/02_Variables/name.rb
+++ Ruby book/02_Variables/name.rb
@@ -1,8 +1,13 @@
 puts 'Hello, type your first name, please!'
 name = gets.chomp!.capitalize!
 
+puts 'Please, enter you last name.'
+last_name = gets.chomp!.capitalize!
+
 puts "Hi, #{name} have a great day."
 
 10.times do
   puts name
 end 
+
+puts "Hi again, #{name} #{last_name}"
