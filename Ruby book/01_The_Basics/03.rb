585d2c44eb9299cebb5fa07dcc0f7e85296bea93
PedroLages <pedrocamposlages@gmail.com>
2021-02-25 16:45:41 +0100

    Exercisse 03 accomplish

---
+++ Ruby book/01_The_Basics/03.rb
@@ -0,0 +1,11 @@
+# Write a program that uses a hash to store a list of movie titles with the year they came out. Then use the puts command to make your program print out the year of each movie to the screen.
+
+movies = { 
+          :The_Departe => 2006,
+          :Man_Bites_Dog => 1992,
+          :The_Godfather => 1972 
+        }
+
+puts movies[:The_Departe]
+puts movies[:Man_Bites_Dog]
+puts movies[:The_Godfather]
