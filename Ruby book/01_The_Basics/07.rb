bdcde6613de0b7d1582b40dedf82642d1252a271
PedroLages <pedrocamposlages@gmail.com>
2021-02-25 16:57:50 +0100

    Exercisse 07 accomplish

---
+++ Ruby book/01_The_Basics/07.rb
@@ -0,0 +1,5 @@
+# What does the following error message tell you?
+# SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+#   from /usr/local/rvm/rubies/ruby-2.5.3/bin/irb:16:in `<main>'
+
+# The following error means we have a syntax error. The ruby interpretation system is wating to see '}' and not´t ')'
